db124ac5b7815e1a05e4226ebe677cd576252b22
Smitty81-webs <82186230+Smitty81-webs@users.noreply.github.com>
2025-01-27 18:14:41 -0600

    Final Kelvin project

--- main.js
+++ main.js
@@ -3,8 +3,10 @@ const kelvin = 293;
 // This code creates a variable called celsius, and we get the celsius value by subtracting 273 from 293.
 var celsius = kelvin - 273;
 // This variable converts celsius to fahrenheit
-var fahrenheit = celsius * (9/5) + 32;
-console.log(`The temperature is ${kelvin} degrees Kelvin`);
+var fahrenheit = celsius * 9/5 + 32;
+var newton = celsius * 33/100;
+console.log(`The temperature is ${kelvin} Kelvins`);
 // Math.floor will round a decimal down to the nearest integer.
 console.log(`The temerature is ${Math.floor(celsius)} degrees Celsius`);
 console.log(`The temperature is ${Math.floor(fahrenheit)} degrees Fahrenheit.`);
+console.log(`The temperature is ${Math.floor(newton)} Newtons`);
